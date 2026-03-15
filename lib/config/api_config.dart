ba01b775f9b60bbdf954740ef45ec8de51e73376
kingzolas <rianvitor309@gmail.com>
2026-03-15 10:30:28 -0300

    aj

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -5,17 +5,17 @@ class ApiConfig {
   // Use o IP da sua máquina na rede se estiver testando em um celular físico
   // Use localhost (ou 127.0.0.1) se estiver rodando no Windows/Linux/Web
 
-  static const String apiUrl = '$baseUrl/api';
+  // static const String apiUrl = '$baseUrl/api';
 
-  static const String baseUrl =
-      'https://school-management-api-76ef.onrender.com';
-  // Esta é a URL para o WebSocket (WSS = WebSocket Seguro)
+  // static const String baseUrl =
+  //     'https://school-management-api-76ef.onrender.com';
+  // // Esta é a URL para o WebSocket (WSS = WebSocket Seguro)
 
   static const String wsUrl = 'wss://school-management-api-76ef.onrender.com';
 
-  // static const String baseUrl =
-  //     'https://weightiest-ironically-marta.ngrok-free.dev';
+  static const String baseUrl =
+      'https://weightiest-ironically-marta.ngrok-free.dev';
 
-  // static const String apiUrl = '$baseUrl/api';
+  static const String apiUrl = '$baseUrl/api';
   // static const String wsUrl = 'ws://localhost:3000';
 }
